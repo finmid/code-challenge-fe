6e23547019f8cc64125bd0e6af95eaa25255be11
Erik Pastor Rios <erikpastorrios1994@gmail.com>
2022-11-03 16:08:20 +0100

    AuthController makes API crash

--- packages/api/src/controllers/AuthController.ts
+++ packages/api/src/controllers/AuthController.ts
@@ -1,24 +1,23 @@
-import { Request, Response } from 'express';
-import { compareSync } from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import { unauthorized } from '@hapi/boom';
-import { SECRET_KEY } from 'src/constants';
-import users from 'src/data/users.json';
+import { Request, Response } from "express";
+import { compareSync } from "bcrypt";
+import jwt from "jsonwebtoken";
+import { SECRET_KEY } from "src/constants";
+import users from "src/data/users.json";
 
 const login = async (req: Request, res: Response) => {
   const email = req.body.email;
   const password = req.body.password;
-  const INCORRECT_LOGIN = 'Incorrect login or password';
+  const INCORRECT_LOGIN = "Incorrect login or password";
 
   const user = users.find((user) => user.email === email);
 
   if (!user) {
-    throw unauthorized(INCORRECT_LOGIN);
+    res.status(401).json({ error: "Unauthorized", message: INCORRECT_LOGIN });
   } else if (!compareSync(password, user.password)) {
-    throw unauthorized(INCORRECT_LOGIN);
+    res.status(401).json({ error: "Unauthorized", message: INCORRECT_LOGIN });
   } else {
     const { password, ...userData } = user;
-    const token = jwt.sign({ userData }, SECRET_KEY, { expiresIn: '1y' });
+    const token = jwt.sign({ userData }, SECRET_KEY, { expiresIn: "1y" });
 
     res.status(200).json({ token });
   }
