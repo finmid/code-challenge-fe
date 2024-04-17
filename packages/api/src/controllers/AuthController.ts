3b5105d480a9d5dd215084f74d4b6ddf517f66ac
Jan Korenek <jan.korenek@deliveryhero.com>
2024-04-17 12:03:37 +0200

    catch async express error (#8)

--- packages/api/src/controllers/AuthController.ts
+++ packages/api/src/controllers/AuthController.ts
@@ -1,26 +1,30 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { compareSync } from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { unauthorized } from '@hapi/boom';
 import { SECRET_KEY } from 'src/constants';
 import users from 'src/data/users.json';
 
-const login = async (req: Request, res: Response) => {
+const login = async (req: Request, res: Response, next: NextFunction) => {
   const email = req.body.email;
   const password = req.body.password;
   const INCORRECT_LOGIN = 'Incorrect login or password';
 
   const user = users.find((user) => user.email === email);
 
-  if (!user) {
-    throw unauthorized(INCORRECT_LOGIN);
-  } else if (!compareSync(password, user.password)) {
-    throw unauthorized(INCORRECT_LOGIN);
-  } else {
-    const { password, ...userData } = user;
-    const token = jwt.sign({ userData }, SECRET_KEY, { expiresIn: '1y' });
+  try {
+    if (!user) {
+      throw unauthorized(INCORRECT_LOGIN);
+    } else if (!compareSync(password, user.password)) {
+      throw unauthorized(INCORRECT_LOGIN);
+    } else {
+      const { password, ...userData } = user;
+      const token = jwt.sign({ userData }, SECRET_KEY, { expiresIn: '1y' });
 
-    res.status(200).json({ token });
+      res.status(200).json({ token });
+    }
+  } catch (error) {
+    next(error)
   }
 };
 
